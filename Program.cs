1f58c29122dd17cebe72971a709431cf664f9fb8
Badflar <badflar2000@gmail.com>
2020-08-07 10:23:22 -0700

    Comments and Bug Fix
    Added:
    - Comments to explain the process
    
    Fixed: 
    - ETH account variable looking up ETC account. 
    - Missing newline in Buy/Sell console logs.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Coinbase.Pro.Models;
 using CoinbasePro;
 using CoinbasePro.Network.Authentication;
 using CoinbasePro.Shared.Types;
-using CoinbasePro.WebSocket.Types;
-using Flurl.Util;
 using Newtonsoft.Json;
 
 
@@ -17,42 +12,56 @@ namespace CryptoTradeBot
 {
     class Program
     {
+        // Interval in seconds   [secs] * [milli to seconds]
         public static int interval = 30 * 1000;
 
+        // Object of keys from Coinbase Pro API
         public static KEYS Keys;
 
+        // Status holders
         public bool isNextOperationBuy = true;
-        public bool isCurrentBTC = true;
+        public bool isCurrentBTC = false; // Dictates whether money is in ETH or BTC
 
+
+        // Buying thresholds 
         public double DIP_THREHOLD = -2.25;
         public double UPWARD_TREND_THRESHOLD = 1.5;
 
+        // Selling thesholds
         public double PROFIT_THRESHOLD = 1.25;
         public double STOP_LOSS_THRESHOLD = -2.00;
 
+        // Price from currency from previous operation
         public double lastOpPriceBTC = 11805.73;
-        public double lastOpPriceETH = 396.57;
+        public double lastOpPriceETH = 376.24;
 
 
         static async Task Main(string[] args)
         {
+            // Call non-static methods
             Program program = new Program();
 
+            // Convert JSON to KEYS object
             using (StreamReader r = new StreamReader(@"E:\_BlueFoxDen\Programs\CryptoTradeBot\CryptoTradeBot\Keys.json"))
             {
                 string json = r.ReadToEnd();
                 Keys = JsonConvert.DeserializeObject<KEYS>(json);
             }
 
+            // Create authenticator
             var authenticator = new Authenticator(Keys.apikey, Keys.apisecret, Keys.apiphrase);
 
+            // Start coinbase client
             var coinbaseProClient = new CoinbaseProClient(authenticator);
 
-            var allAccounts = await coinbaseProClient.AccountsService.GetAllAccountsAsync();
 
+            // Used to get IDs of profiles and get balances for debugging
+            //var allAccounts = await coinbaseProClient.AccountsService.GetAllAccountsAsync();
             //foreach(var account in allAccounts)
             //Console.WriteLine(account.Id + " " + account.Currency + " " + account.Balance);
 
+
+            // Main loop
             while (true)
             {
                 await program.MakeTrade(coinbaseProClient);
@@ -62,6 +71,7 @@ namespace CryptoTradeBot
         
         private async Task MakeTrade(CoinbaseProClient coinbaseProClient)
         {
+            // Get percent differences and current prices of currencies
             var currentTickerBTC = await coinbaseProClient.ProductsService.GetProductTickerAsync(ProductType.BtcUsd);
             double currentPriceBTC = (double)currentTickerBTC.Price;
             var percentDiffBTC = (currentPriceBTC - lastOpPriceBTC) / lastOpPriceBTC * 100;
@@ -70,8 +80,11 @@ namespace CryptoTradeBot
             double currentPriceETH = (double)currentTickerETH.Price;
             var percentDiffETH = (currentPriceETH - lastOpPriceETH) / lastOpPriceETH * 100;
 
+            // Check if next action is to buy
             if (isNextOperationBuy == true) 
             {
+                // TODO - Check both percent differences and pick the "better outcome" percent
+                // Check if BTC percent difference is either greater than upward trend (spike upwards) or if percent difference is less than dip (negative number) 
                 if (percentDiffBTC >= UPWARD_TREND_THRESHOLD || percentDiffBTC <= DIP_THREHOLD)
                 {
                     var USDAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("a8a83415-dc6c-4327-bf84-b3eb9859d81d");
@@ -79,19 +92,20 @@ namespace CryptoTradeBot
                     await coinbaseProClient.OrdersService.PlaceMarketOrderAsync(CoinbasePro.Services.Orders.Types.OrderSide.Buy, ProductType.BtcUsd, Math.Round(USDAccount.Balance, 2), CoinbasePro.Services.Orders.Types.MarketOrderAmountType.Size);
                     lastOpPriceBTC = currentPriceBTC;
                     var BTCAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("4fb67e84-e6e9-46fd-bac4-ea03dc242c61");
-                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Buy\n[Price] = " + currentPriceBTC.ToString() + "\n[BTC Avaliable] = " + BTCAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Balance.ToString() + "[Percent Difference] = " + percentDiffBTC.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Buy\n[Price] = " + currentPriceBTC.ToString() + "\n[BTC Avaliable] = " + BTCAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Balance.ToString() + "\n[Percent Difference] = " + percentDiffBTC.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
                     isNextOperationBuy = false;
                     isCurrentBTC = true;
                 }
+                // Check if ETH percent difference is either greater than upward trend (spike upwards) or if percent difference is less than dip (negative number) 
                 else if (percentDiffETH >= UPWARD_TREND_THRESHOLD || percentDiffETH <= DIP_THREHOLD)
                 {
                     var USDAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("a8a83415-dc6c-4327-bf84-b3eb9859d81d");
                     Console.WriteLine(USDAccount.Balance);
                     await coinbaseProClient.OrdersService.PlaceMarketOrderAsync(CoinbasePro.Services.Orders.Types.OrderSide.Buy, ProductType.EthUsd, Math.Round(USDAccount.Balance, 2), CoinbasePro.Services.Orders.Types.MarketOrderAmountType.Size);
                     lastOpPriceETH = currentPriceETH;
-                    var ETHAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("e67d239b-1ca8-4eaa-a288-3de872439a81");
-                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Buy\n[Price] = " + currentPriceETH.ToString() + "\n[ETH Avaliable] = " + ETHAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Balance.ToString() + "[Percent Difference] = " + percentDiffETH.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    var ETHAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("da8129ff-84e3-4612-8ec7-40eeee6c55ad");
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Buy\n[Price] = " + currentPriceETH.ToString() + "\n[ETH Avaliable] = " + ETHAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Balance.ToString() + "\n[Percent Difference] = " + percentDiffETH.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
                     isNextOperationBuy = false;
                     isCurrentBTC = false;
@@ -101,30 +115,36 @@ namespace CryptoTradeBot
             }
             else if (isNextOperationBuy == false)
             {
+                // If money is put into BTC...
                 if (isCurrentBTC == true)
                 {
+                    // And if current BTC price is going up or going down drastically...
                     if (percentDiffBTC >= PROFIT_THRESHOLD || percentDiffBTC <= STOP_LOSS_THRESHOLD)
                     {
+                        // Then sell
                         var BTCAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("4fb67e84-e6e9-46fd-bac4-ea03dc242c61");
                         Console.WriteLine(BTCAccount.Balance);
                         var trade = await coinbaseProClient.OrdersService.PlaceMarketOrderAsync(CoinbasePro.Services.Orders.Types.OrderSide.Sell, ProductType.BtcUsd, Math.Round(BTCAccount.Balance, 2), CoinbasePro.Services.Orders.Types.MarketOrderAmountType.Size);
                         lastOpPriceBTC = currentPriceBTC;
                         var USDAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("a8a83415-dc6c-4327-bf84-b3eb9859d81d");
-                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Sell\n[Price] = " + currentPriceBTC.ToString() + "\n[BTC Avaliable] = " + BTCAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Available.ToString() + "[Percent Difference] = " + percentDiffBTC.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Sell\n[Price] = " + currentPriceBTC.ToString() + "\n[BTC Avaliable] = " + BTCAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Available.ToString() + "\n[Percent Difference] = " + percentDiffBTC.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
                         isNextOperationBuy = true;
                     }
                 }
+                // Else if money is put into ETH...
                 else if (isCurrentBTC == false)
                 {
+                    // And if current ETH price is going up or going down drastically...
                     if (percentDiffETH >= PROFIT_THRESHOLD || percentDiffETH <= STOP_LOSS_THRESHOLD)
                     {
-                        var ETHAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("e67d239b-1ca8-4eaa-a288-3de872439a81");
+                        // Then sell
+                        var ETHAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("da8129ff-84e3-4612-8ec7-40eeee6c55ad");
                         Console.WriteLine(ETHAccount.Balance);
-                        var trade = await coinbaseProClient.OrdersService.PlaceMarketOrderAsync(CoinbasePro.Services.Orders.Types.OrderSide.Sell, ProductType.BtcUsd, Math.Round(ETHAccount.Balance, 2), CoinbasePro.Services.Orders.Types.MarketOrderAmountType.Size);
+                        var trade = await coinbaseProClient.OrdersService.PlaceMarketOrderAsync(CoinbasePro.Services.Orders.Types.OrderSide.Sell, ProductType.EthUsd, Math.Round(ETHAccount.Balance, 2), CoinbasePro.Services.Orders.Types.MarketOrderAmountType.Size);
                         lastOpPriceETH = currentPriceETH;
                         var USDAccount = await coinbaseProClient.AccountsService.GetAccountByIdAsync("a8a83415-dc6c-4327-bf84-b3eb9859d81d");
-                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Sell\n[Price] = " + currentPriceETH.ToString() + "\n[ETH Avaliable] = " + ETHAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Available.ToString() + "[Percent Difference] = " + percentDiffETH.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" + "[Operation] = Sell\n[Price] = " + currentPriceETH.ToString() + "\n[ETH Avaliable] = " + ETHAccount.Balance.ToString() + "\n[USD Avaliable] = " + USDAccount.Available.ToString() + "\n[Percent Difference] = " + percentDiffETH.ToString() + "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
                         isNextOperationBuy = true;
                     }
